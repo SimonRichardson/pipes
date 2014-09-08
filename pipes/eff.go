2bffcff7012af946e5f1a1dff1bd7ea99f211f24
Simon Richardson <simon@dice.fm>
2014-09-08 21:17:32 +0100

    More effective functions

--- pipes/eff.go
+++ pipes/eff.go
@@ -6,3 +6,16 @@ func Eff(command Command) func(Note) Either {
 		return EitherFromBool(res.Continue, NewTuple([]Note{note}, res.Note))
 	}
 }
+
+func EffF(f func(Note) CommandResult) func(Note) Either {
+	return func(note Note) Either {
+		res := f(note)
+		return EitherFromBool(res.Continue, NewTuple([]Note{note}, res.Note))
+	}
+}
+
+func EffM(f func(Note) Note) func(Note) Either {
+	return func(note Note) Either {
+		return NewRight(NewTuple([]Note{note}, f(note)))
+	}
+}
